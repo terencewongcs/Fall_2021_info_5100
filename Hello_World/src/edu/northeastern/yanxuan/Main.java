26a982b8e6f55326d9d5bc14c23fef47a1436f02
Teri Wang <wang.yanxu@northeastern.edu>
2021-09-14 21:21:31 -0700

    checking in code for adding primitive data types

---
+++ Hello_World/src/edu/northeastern/yanxuan/Main.java
@@ -0,0 +1,23 @@
+package edu.northeastern.yanxuan;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int num=62;
+        Integer test=52;
+
+        Integer abc=new Integer(4);
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
+        System.out.println("MAX"+max);
+        System.out.println("MIN"+min);
+
+        char ch='A';
+        System.out.println(ch+1);
+
+
+        System.out.println("Hello World");
+    }
+
+}
