b2c02f6c4275181036bdb3c2891afc2e9f8a614f
Teri Wang <wang.yanxu@northeastern.edu>
2021-09-28 18:09:04 -0700

    add files

--- OperatorsAndControlFlow/src/edu/northeastern/yanxuan/Main.java
+++ OperatorsAndControlFlow/src/edu/northeastern/yanxuan/Main.java
@@ -32,8 +32,31 @@ public class Main {
         System.out.println("a = "+a);
         System.out.println("b = "+b);
 
-        if(bValue == true){
-
+        if(bValue) {
+            System.out.println("Boolean value is true.");
         }
+
+        if( b > 10 ) {
+            System.out.println("b's value is greater than 10.");
+        }else{
+            System.out.println("b's value is less than 10.");
+        }
+
+        // result = (conditional statement)? value 1 : value 2
+
+        //1: 0000 0001
+        //-1: 1111 1111
+        int result = b > 10 ? 10 :30;
+
+        Integer x=Integer.MAX_VALUE;
+        int y=15;
+        int z=x^y;
+        System.out.printf("%08x^%08x=%08x\n",x,y,z);
+        System.out.println(z);
+
+
+        String namename="name1";
+        namename="name2";
+        System.out.println(namename);
     }//end of main function
 }//end of the class
