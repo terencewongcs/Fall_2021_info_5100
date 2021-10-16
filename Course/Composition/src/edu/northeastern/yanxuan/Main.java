f29c2f38a1eeb784ccdd145c1bca02dd16ebf76c
Yanxuan Wang <511085063@qq.com>
2021-10-15 19:51:04 -0700

    Homework 2 Question 1

---
+++ Course/Composition/src/edu/northeastern/yanxuan/Main.java
@@ -0,0 +1,11 @@
+package edu.northeastern.yanxuan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car tsla=new Car("c300",17,true,4,true);
+
+        tsla.getGearBox().setCurrentGear(1);
+    }
+}
