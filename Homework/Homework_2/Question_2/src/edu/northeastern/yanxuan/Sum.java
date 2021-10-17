0915bb664495d208ad3ac5885314b2171ae48a4f
Yanxuan Wang <511085063@qq.com>
2021-10-16 20:42:42 -0700

    Homework 2 Question 2

---
+++ Homework/Homework_2/Question_2/src/edu/northeastern/yanxuan/Sum.java
@@ -0,0 +1,24 @@
+package edu.northeastern.yanxuan;
+
+public class Sum {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public double add(double a,int b){
+        return a+b;
+    }
+
+    public double add(int a,double b){
+        return a+b;
+    }
+
+    public double add(double a,double b){
+        return a+b;
+    }
+
+    public int add(int a,int b, int c){
+        return a+b+c;
+    }
+}
